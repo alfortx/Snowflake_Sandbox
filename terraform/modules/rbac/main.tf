b6aafb0fe59f75cf84b6f8ab141ed6d39f97405c
alfortx <alfortx@yahoo.co.jp>
2026-04-20 21:08:32 +0900

    test: pre-commitフック動作確認用（後でrevert）

--- terraform/modules/rbac/main.tf
+++ terraform/modules/rbac/main.tf
@@ -120,3 +120,4 @@ resource "snowflake_grant_account_role" "viewer_inherits_raw_company_matching_re
   role_name        = snowflake_account_role.fr_raw_company_matching_read.name
   parent_role_name = var.viewer_role_name
 }
+
